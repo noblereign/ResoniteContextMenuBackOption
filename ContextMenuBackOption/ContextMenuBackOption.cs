e977213e3a4d3ab38596e96bb0cd2517eb7f1f12
Noble <noblereign@users.noreply.github.com>
2026-08-27 20:19:19 -0700

    version bump

--- ContextMenuBackOption/ContextMenuBackOption.cs
+++ ContextMenuBackOption/ContextMenuBackOption.cs
@@ -17,7 +17,7 @@ namespace ContextMenuBackOption;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
 //Mod code partially based on https://github.com/XDelta/ResoniteFish (because it's a very simple context menu option mod lol)
 public class ContextMenuBackOption : ResoniteMod {
-	internal const string VERSION_CONSTANT = "4.0.0";
+	internal const string VERSION_CONSTANT = "4.0.1";
 	public override string Name => "ContextMenuBackOption";
 	public override string Author => "Noble";
 	public override string Version => VERSION_CONSTANT;
